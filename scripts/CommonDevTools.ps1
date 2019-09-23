1e7dc70e0a7b2eb0858b83f9a5efad0580143293
Sinhk <sindre.kroknes@gmail.com>
2019-09-23 20:48:56 +0200

    removed posh-git

--- scripts/CommonDevTools.ps1
+++ scripts/CommonDevTools.ps1
@@ -4,7 +4,6 @@ choco install -y vscode
 choco install -y powershell-core
 choco install -y git
 choco install -y git-lfs
-choco install -y poshgit
 choco install -y python
 choco install -y 7zip.install
 choco install -y sysinternals
